935a1a5716f1ef87ba86f16b751d9fa9dfbea852
Prak-NodeJs <111414192+Prak-NodeJs@users.noreply.github.com>
2023-10-17 11:15:37 +0530

    Update condfig.js

--- backend/DB/condfig.js
+++ backend/DB/condfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 const DB =
-  'mongodb+srv://prakash:prak1234@cluster0.nbtkiwp.mongodb.net/commerce?retryWrites=true&w=majority'
+  ''
 
 const con = mongoose.connect(DB).then(() => console.log('connected'))
